6bfe7b08b2c87564d3dd44153df6c363df57a9d6
sabrinacafe <sabis476@gmail.com>
2024-11-17 01:46:00 -0300

    Adiciona projeto EcoDrive

---
+++ src/test/java/com/java/EcoDrive/EcoDriveApplicationTests.java
@@ -0,0 +1,14 @@
+/*package com.java.EcoDrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}*/
+
