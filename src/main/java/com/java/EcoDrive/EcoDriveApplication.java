7246bd1d86615bdb43450909fbb5a7bad8111202
sabrinacafe <sabis476@gmail.com>
2024-11-19 04:20:09 -0300

    TestandoAPI no swagger

--- src/main/java/com/java/EcoDrive/EcoDriveApplication.java
+++ src/main/java/com/java/EcoDrive/EcoDriveApplication.java
@@ -2,13 +2,12 @@ package com.java.EcoDrive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.java")
 public class EcoDriveApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EcoDriveApplication.class, args);
 	}
 }
+
 
